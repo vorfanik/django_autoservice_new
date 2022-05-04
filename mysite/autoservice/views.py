eb27392e6e04e38db55ddf483ab247236a149b6e
Nikita <nikitavornik@gmail.com>
2022-05-04 14:35:00 +0300

    update

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import AutoModel, Service, Car, Order, OrderingLine
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    services = Service.objects.count()
+    completed_services = Order.objects.filter(status__exact='com').count()
+    cars = Car.objects.count()
+
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'services': services,
+        'completed_services': completed_services,
+        'cars': cars,
+    }
+
+    # renderiname base.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context=context)
